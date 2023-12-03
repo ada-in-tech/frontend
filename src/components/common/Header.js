e04fb82db5083c43973fd02d2c9e8347be19c98f
Tito-Olisakwe <t.olisakwe@alustudent.com>
2023-12-03 19:10:54 +0200

    fixed headers

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navigation from './Navigation';
 import { useUser } from '../../contexts/UserContext';
 import '../../styles/components.css';
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { isSignedIn, userType } = useUser(); // Use the context
-
-    const toggleMenu = () => {
-        setIsMenuOpen(prevState => !prevState);
-    };
 
     return (
         <header className="header">
             <div className="logo">ADA IN TECH</div>
-            <button className="hamburger" onClick={toggleMenu}>
-                <span>☰</span>
-            </button>
-            <nav className={`navigation ${isMenuOpen ? 'expanded' : ''}`}>
+            <nav className="navigation">
                 <Navigation userType={isSignedIn ? userType : 'guest'} />
             </nav>
         </header>
